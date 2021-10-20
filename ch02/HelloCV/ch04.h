25632f4c328b8afea4d10157bfb597cc638dbd51
Lee-Seungwook <lsw971226@naver.com>
2021-10-20 13:58:55 +0900

    비디오 기능 합침

---
+++ ch02/HelloCV/ch04.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "opencv2/opencv.hpp"
+
+void camera_in();
+
+void video_in();
+
+void camera_in_video_out();
--- ch02/HelloCV/main.cpp
+++ ch02/HelloCV/main.cpp
@@ -1,30 +1,32 @@
 #include "opencv2/opencv.hpp"
 #include "iostream"
 
+#include "ch04.h"
+
 using namespace std;
 using namespace cv;
 
 int main()
 {
-	cout << "Hello OpenCV" << CV_VERSION << endl;
+	//cout << "Hello OpenCV" << CV_VERSION << endl;
 
 
 
-	Mat img;
-	img = imread("lenna.bmp"); // 이미지 불러오기
+	//Mat img;
+	//img = imread("lenna.bmp"); // 이미지 불러오기
 
-	if (img.empty()) { // 객체가 비었는지(데이터가 정상적으로 불러왔는지 판단)
-		cerr << "Image load failed!" << endl;
-		return -1;
-	}
+	//if (img.empty()) { // 객체가 비었는지(데이터가 정상적으로 불러왔는지 판단)
+	//	cerr << "Image load failed!" << endl;
+	//	return -1;
+	//}
 
-	namedWindow("image"); // image라는 이름으로 영상을 출력할 빈 창을 생성
+	//namedWindow("image"); // image라는 이름으로 영상을 출력할 빈 창을 생성
 
-	//moveWindow("image", 20, 20); // 영상 출력 창을 x, y 좌표로 이동
-	//resizeWindow("image", 256, 256); // 영상 출력 창의 크기를 변경
+	////moveWindow("image", 20, 20); // 영상 출력 창을 x, y 좌표로 이동
+	////resizeWindow("image", 256, 256); // 영상 출력 창의 크기를 변경
 
-	imshow("image", img); // 영상을 화면에 출력
-	waitKey(); // 키보드 입력을 받는 용도로 사용, waitkey까지 호출해야 화면에 영상이 나타남
+	//imshow("image", img); // 영상을 화면에 출력
+	//waitKey(); // 키보드 입력을 받는 용도로 사용, waitkey까지 호출해야 화면에 영상이 나타남
 
 	// jpeg 파일로 압축률 지정하여 저장
 	/*vector<int> params;
@@ -38,8 +40,10 @@ int main()
 	// destroyWindow("image"); // 영상 출력 창을 닫는다. 
 	// destroyAllWindows(); // 모든 영상 출력창을 닫는다.
 	
-
+	//camera_in(); // 컴퓨터 카메라
 	
+	// video_in(); // 비디오 재생
 
+	// void camera_in_video_out(); // 카메라로 얻은 영상을 저장
 	return 0;
 }
